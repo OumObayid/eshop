0f37f5a52f4a58f43664709b9fdb8b8a83d1e4dd
OumObayid <elobayidoumaima@gmail.com>
2022-12-21 02:58:37 +0100

    next

--- src/components/stars/Stars.js
+++ src/components/stars/Stars.js
@@ -4,8 +4,9 @@ import { setRating } from "../../redux/dataSlice";
 import { useDispatch } from "react-redux";
 import { db } from "../../firebase/config";
 import { doc, updateDoc } from "firebase/firestore";
-import {RiArrowDropDownLine} from "react-icons/ri"
-
+import { RiArrowDropDownLine } from "react-icons/ri";
+import Tooltip from "react-tooltip-lite";
+import "./Stars.css";
 const Stars = (props) => {
   const [rat, setRat] = useState(props.stars);
   const [vote, setvote] = useState(props.vote);
@@ -34,22 +35,102 @@ const Stars = (props) => {
   let res = Math.round(props.stars * 10) / 10;
   return (
     <div className="d-flex justify-content-start  mt-3 ">
-      <div className=" mb-2 me-2 d-flex align-items-end ">
-        <StarRatings
-          rating={props.stars}
-          isSelectable={true}
-          starRatedColor={"#FFCA2C"}
-          starHoverColor={"#D48443"}
-          starDimension={"16px"}
-          starSpacing={"1px"}
-          isAggregateRating={false}
-          changeRating={changeRating}
-          numOfStars={6}
-        />       
-      </div>
-      <span className="fs-4 d-flex align-items-end  ">{res}<RiArrowDropDownLine size={20}/></span>
-        
-      <span className="fs-4  d-flex align-items-end ">      
+      <Tooltip
+        background="white"
+        color="black"
+        tipContentClassName="arrow-content-tooltip"
+        arrow={true}
+        direction="down"
+        arrowContent={
+          <svg
+            style={{ display: "block" }}
+            viewBox="0 0 21 11"
+            width="20px"
+            height="10px"
+          >
+            <path
+              d="M0,11 L9.43630703,1.0733987 L9.43630703,1.0733987 C10.1266203,0.3284971 11.2459708,0 11.936284,1.0733987 L20,11"
+              style={{ stroke: "gray", fill: "white" }}
+            />
+          </svg>
+        }
+        content={
+          <ul className="customer-reviews">
+            <li className="customer-reviews-item flex">
+              <div className="customer-reviews-stars">5 Stars </div>
+              <div className="customer-reviews-slide">
+                <span
+                  className="customer-reviews-score"
+                  style={{ width: "81%" ,backgroundColor:"red"}}
+                ></span>
+              </div>
+              <div className="customer-reviews-per">81%</div>
+            </li>
+            <li className="customer-reviews-item flex">
+              <div className="customer-reviews-stars">4 Stars </div>
+              <div className="customer-reviews-slide">
+                <span
+                  className="customer-reviews-score"
+                  style={{ width: "12%" }}
+                ></span>
+              </div>
+              <div className="customer-reviews-per">12%</div>
+            </li>
+            <li className="customer-reviews-item flex">
+              <div className="customer-reviews-stars">3 Stars </div>
+              <div className="customer-reviews-slide">
+                <span
+                  className="customer-reviews-score"
+                  style={{ width: "3%" }}
+                ></span>
+              </div>
+              <div className="customer-reviews-per">3%</div>
+            </li>
+            <li className="customer-reviews-item flex">
+              <div className="customer-reviews-stars">2 Stars </div>
+              <div className="customer-reviews-slide">
+                <span
+                  className="customer-reviews-score"
+                  style={{ width: "1%" }}
+                ></span>
+              </div>
+              <div className="customer-reviews-per">1%</div>
+            </li>
+            <li className="customer-reviews-item flex">
+              <div className="customer-reviews-stars">1 Stars </div>
+              <div className="customer-reviews-slide">
+                <span
+                  className="customer-reviews-score"
+                  style={{ width: "3%" }}
+                ></span>
+              </div>
+              <div className="customer-reviews-per">3%</div>
+            </li>
+          </ul>
+        }
+      >
+        <div className="d-flex justify-content-start  mt-3 ">
+          <div className=" mb-2 me-2 d-flex align-items-end ">
+            <StarRatings
+              rating={props.stars}
+              isSelectable={true}
+              starRatedColor={"#FFCA2C"}
+              starHoverColor={"#D48443"}
+              starDimension={"16px"}
+              starSpacing={"1px"}
+              isAggregateRating={false}
+              changeRating={changeRating}
+              numOfStars={6}
+            />
+          </div>
+          <span className="fs-4 d-flex align-items-end  ">
+            {res}
+            <RiArrowDropDownLine size={20} />
+          </span>
+        </div>
+      </Tooltip>
+
+      <span className="fs-4 ms-5 d-flex align-items-end ">
         {props.vote} Reviews
       </span>
     </div>
