021dc4793ad861d19bfc1abeaf5705d2caa69ecf
OumObayid <elobayidoumaima@gmail.com>
2022-12-24 02:36:06 +0100

    next

--- src - 4/components/Helmet/Helmet.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Helmet = (props) => {
-  document.title = "eShop ordering app - " + props.title;
-  return <div style={{marginTop: "5rem"}} className="container w-100">{props.children}</div>;
-};
-
-export default Helmet;
